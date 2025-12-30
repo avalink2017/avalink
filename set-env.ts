cc423c8552ef6b4bc3a1f278485bca593bd109e4
Fernando Reyes <avalink2017@gmail.com>
2025-12-29 21:17:01 -0600

    AvaLink - 29122025-2

--- set-env.ts
+++ set-env.ts
@@ -1,13 +1,15 @@
 const fs = require('fs');
+// Cambiamos la ruta para que afecte específicamente al archivo de producción
 const targetPath = './src/environments/environment.ts';
 
 const envConfigFile = `
 export const environment = {
   production: true,
-  apiUrl: '${process.env['API_URL']}',
-  apiKey: '${process.env['API_KEY']}',
-  turnstileKey: '${process.env['TURNSTILE_SITE_KEY']}'
+  apiUrl: '${process.env['API_URL'] || ''}',
+  apiKey: '${process.env['API_KEY'] || ''}',
+  turnstileKey: '${process.env['TURNSTILE_SITE_KEY'] || ''}'
 };
 `;
 
 fs.writeFileSync(targetPath, envConfigFile);
+console.log('✅ environment.ts generado dinámicamente');
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: (window as any).env?.API_URL || '',
-  apiKey: (window as any).env?.API_KEY || '',
-  turnstileKey: (window as any).env?.TURNSTILE_SITE_KEY || '',
+  apiUrl: '',
+  apiKey: '',
+  turnstileKey: '',
 };
